8ef5887ba116d5210c3c7f5639bef0395d7c8bfe
Andrii Demkiv <andriydemkiv14@gmail.com>
2025-12-20 20:59:21 +0100

    Impored the main page UI, change theme colors

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,12 @@ import { Logo } from "@/components/ui/logo";
 import {
   BadgeCheck,
   CodeXml,
-  FingerprintPattern,
+  Component,
+  Database,
+  Paintbrush,
   Rocket,
-  Usb
+  ShieldCheck,
 } from "lucide-react";
-
 
 const logos = [
   {
@@ -46,6 +47,32 @@ const logos = [
   },
 ]
 
+const iconSize = 64
+
+const cards = [
+  {
+    name: "Polished UI",
+    description: "A clean, modern interface out of the box. Easy to customize, easy to extend — focused on shipping, not styling from scratch.",
+    icon: <Paintbrush size={iconSize} />
+  }, {
+    name: "Flexible components",
+    description: "Accessible, scalable, reusable components you own — integrate them into any app in minutes.",
+    icon: <Component size={iconSize} />
+  }, {
+    name: "Type Safety",
+    description: "Schemas define validation for forms and API calls — fewer bugs, no guesswork.",
+    icon: <BadgeCheck size={iconSize} />
+  }, {
+    name: "PostgreSQL Database",
+    description: "A powerful, open-source backend-as-a-service database with built-in auth, real-time subscriptions, and migrations — ideal for fast building and scaling.",
+    icon: <Database size={iconSize} />
+  }, {
+    name: "Secure by default",
+    description: "Built-in authentication flows with row-level security, server-side confirmation, and session handling — keeping your data safe without the struggle.",
+    icon: <ShieldCheck size={iconSize} />
+  },
+]
+
 export default function Home() {
   return (
     <>
@@ -53,24 +80,22 @@ export default function Home() {
         <div className="mx-auto flex w-9/10 flex-wrap justify-center text-center lg:max-w-4xl">
           <div className="w-full justify-between">
             <section id="hero">
-              <h1 className="my-6 text-5xl font-extrabold tracking-tight text-balance md:text-7xl">
-                Production-ready Supabase Authentication
+              <h1 className="my-6 text-5xl text-ellipsis font-extrabold tracking-tight text-balance md:text-7xl">
+                Authentication<span className="text-primary"> built for developers </span>
               </h1>
-              <p className="my-4 text-lg font-light text-balance">
-                A complete, customizable authentication system built for security, scalability, and great user experience — out of the box.
+              <p className="my-4 mx-auto max-w-2/3 text-lg font-semibold text-pretty text-muted-foreground">
+                Stop configuring and start innovating. Securely build, deploy and scale authentication with <span className="text-foreground">row-level security</span>, <span className="text-foreground">type safety </span> and <span className="text-foreground"> React-friendly </span> components.
               </p>
 
               <div className="flex justify-center gap-4 ">
                 <Button className="text-base" cursor="pointer" asChild>
-                  <Link
-                    href="#">
+                  <Link href="#">
                     <Rocket />
                     Get started
                   </Link>
                 </Button>
                 <Button className="text-base" variant="outline" cursor="pointer" asChild>
-                  <Link
-                    href="#">
+                  <Link href="#">
                     <CodeXml />
                     View code
                   </Link>
@@ -79,7 +104,7 @@ export default function Home() {
             </section>
 
             <section id="tools">
-              <div className="items-center my-16 flex justify-around">
+              <div className="my-16 flex items-center justify-around">
                 {logos.map((logo) => (
                   <Logo
                     key={logo.name}
@@ -96,40 +121,28 @@ export default function Home() {
             <section id="solutions">
               <Separator className="my-12 w-1/4" />
 
-              <h6 className="m-4 text-2xl font-semibold text-balance">
-                Everything you need — out of the box
-              </h6>
+              <div className="my-16 space-y-4">
+                <h6 className="text-5xl font-bold text-balance tracking-tight">
+                  Everything you need to ship fast
+                </h6>
+                <p className="text-sm font-semibold text-muted-foreground">
+                  Production-ready authentication with all the features developers need
+                </p>
+              </div>
 
-              <div className="mx-auto my-6 grid max-w-xl gap-6 lg:max-w-full lg:grid-cols-3 ">
-                <Card>
-                  <CardHeader className="text-xl font-extrabold">
-                    Secure authorization
-                  </CardHeader>
-                  <CardContent className="lg:mx-4">Built-in authentication flows with row-leves security, server-side confirmation, session handling, and Supabase best practices — secure by default.</CardContent>
-                  <CardFooter className="mt-auto">
-                    <FingerprintPattern size={32} />
-                  </CardFooter>
-                </Card>
-
-                <Card className="">
-                  <CardHeader className="text-xl font-extrabold">
-                    Auth Wrapper and Provider
-                  </CardHeader>
-                  <CardContent className="mx-4">Preconfigured provider and React-friendly wrapper that make developing auth-depend components simple, predictable, and easy to extend.</CardContent>
-                  <CardFooter className="mt-auto">
-                    <Usb size={32} />
-                  </CardFooter>
-                </Card>
-
-                <Card>
-                  <CardHeader className="text-xl font-extrabold">
-                    Types validation
-                  </CardHeader>
-                  <CardContent className="mx-4">Strongly typed validation for forms and API calls, reducing runtime errors and improving developer confidence.</CardContent>
-                  <CardFooter className="mt-auto">
-                    <BadgeCheck size={32} />
-                  </CardFooter>
-                </Card>
+              <div className="mx-auto my-6 grid gap-2 grid-cols-6 ">
+                {cards.map((card, i) => (
+                  <Card key={card.name}
+                    className={`col-span-full border-3 md:hover:shadow-md md:hover:shadow-primary bg-transparent duration-300 hover:border-primary lg:col-span-2 ${i < 3 ? 'lg:col-span-2' : 'lg:col-span-3'}`}>
+                    <CardHeader className="flex flex-col items-center justify-center text-center text-2xl font-extrabold" >
+                      <div className="mt-2 mb-4 text-primary">{card.icon} </div>
+                      {card.name}
+                    </CardHeader>
+                    <CardContent className="text-pretty">
+                      {card.description}
+                    </CardContent>
+                  </Card>
+                ))}
               </div>
             </section>
           </div>
