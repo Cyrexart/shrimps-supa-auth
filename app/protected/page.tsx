eff1b3c808c79b8cb7703ec9fee7db6d1ddafe8a
Andrii Demkiv <andriydemkiv14@gmail.com>
2025-12-21 17:43:16 +0100

    added profile settings shema

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -4,12 +4,9 @@
 import { useUserProfile } from "@/lib/hooks/use-user-profile";
 
 // ------------ Components ------------
-import { Card, CardContent } from "@/components/ui/card";
-import { LogOutButton } from "@/components/auth/ui/logout-button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import ProfileSettings from "@/components/ui/profile";
 
 // ------------ Icons ------------
-
 
 export default function Page() {
   const data = useUserProfile()
@@ -25,17 +22,10 @@ export default function Page() {
   return (
     <main className="overflow-hidden pt-24 lg:pt-48 ">
       <div className="m-24 flex mx-auto justify-center">
-        <Card>
-          <CardContent>
-            Hello, {data?.name}
-            <Avatar>
-              {profileImage && <AvatarImage src={profileImage} alt={initials} />}
-              <AvatarFallback>{initials}</AvatarFallback>
-            </Avatar>
-            <LogOutButton />
-          </CardContent>
-        </Card>
+        <ProfileSettings
+          profile={{ name: "Cyrex", email: "cyrex@gmail.com" }}
+        />
       </div>
-    </main>
+    </main >
   )
 }
